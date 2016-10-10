3a576177041c87c7cf6f001a0c543158b941edc0
moiz <mmoiz129>
2016-10-10 22:27:37 +0500

    edit title and description

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -8,8 +8,9 @@ class Login extends CI_Controller {
     public function __construct() {
 
         parent::__construct();
-        $this->load->library('facebook', array('appId' => '1817748195152831', 'secret' => '93fd508813778a9dcc2fd64a4c1c05d0'));
+        $this->load->library('facebook', array('appId' => '1746238735630300', 'secret' => 'afed1612b79712ea6b3ead1394bd489e'));
         $this->user = $this->facebook->getUser();
+        $this->load->model('UserModel');
     }
 
     public function index() {
@@ -20,6 +21,21 @@ class Login extends CI_Controller {
             
             $data['user_profile'] = $this->facebook->api('/me/?fields=id,name,email,location');
             $data['logout_url'] = $this->facebook->getLogoutUrl(array('next' => base_url() . 'index.php/oauth_login/logout'));
+
+            $email = $data['user_profile']['email'];
+            $name = $data['user_profile']['name'];
+            $id = $data['user_profile']['id'];
+            
+            if (!$this->UserModel->userExist($email)) {
+
+                $this->UserModel->insert( array(
+                    'email'       => $email,
+                    'name'        => $name,
+                    'facebook_id' => $id
+                ));
+
+            }
+
             $this->session->set_userdata($data);
 
             redirect('home');
@@ -27,7 +43,7 @@ class Login extends CI_Controller {
         } else {
 
             // Store users facebook login url
-            $data['login_url'] = $this->facebook->getLoginUrl();
+            $data['login_url'] = $this->facebook->getLoginUrl(array('scope'=>'email'));
 
             $this->load->view('login', $data);
         }
