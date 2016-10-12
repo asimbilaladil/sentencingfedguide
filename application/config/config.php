64c0bd07d1a45647bf1b677db03b7b1e935466c1
moiz <mmoiz129>
2016-10-12 12:45:57 +0500

    fix

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sentencingfedguide';
+
+//production
+$config['base_url'] = 'http://sentencingfedguide.com/';
+
+//development
+//$config['base_url'] = 'http://localhost/sentencingfedguide';
 
 /*
 |--------------------------------------------------------------------------
